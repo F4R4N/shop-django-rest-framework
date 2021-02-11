674f735c45e968c7ddedc2d7d19a2802dc49dabe
Faran Taghavi <farantgh@gmail.com>
2021-02-11 15:29:05 -0500

    edited routes

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
 from . import views
-
 router = routers.DefaultRouter()
 router.register('product', views.ProductView)
 router.register('user', views.UserView)
