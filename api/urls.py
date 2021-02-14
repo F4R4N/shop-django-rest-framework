2684bf86d51c375847fb65a4f8a38b25f882fb79
Faran Taghavi <farantgh@gmail.com>
2021-02-14 12:31:09 -0500

    handled the balance of product quantity and cartitme quantitys && added add_one and reduce_one views

--- api/urls.py
+++ api/urls.py
@@ -13,8 +13,7 @@ urlpatterns = [
     path('v1/cart/', views.CartItemView.as_view()),
     path('v1/cart/add/', views.CartItemAddView.as_view()),
     path('v1/cart/delete/<int:pk>/', views.CartItemDelView.as_view()),
-    # TODO: add serializers and views for fallowing urls
-    path('v1/cart/add_one/<int:pk>/', views.CartItemAddOne),
-    path('v1/cart/reduce_one/<int:pk>', views.CartItemReduceOne),
+    path('v1/cart/add_one/<int:pk>/', views.CartItemAddOneView.as_view()),
+    path('v1/cart/reduce_one/<int:pk>/', views.CartItemReduceOneView.as_view()),
 ]
 
--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions, generics, status
+from rest_framework import viewsets, permissions, generics, status, exceptions
 from .models import Product, Category, CartItem, Profile
 from .serializers import ProductSerializer, CategorySerializer, CartItemSerializer, UserSerializer, CartItemAddSerializer
 from django.contrib.auth.models import User
@@ -37,6 +37,58 @@ class CartItemAddView(generics.CreateAPIView):
     serializer_class = CartItemAddSerializer
     permission_classes = (permissions.IsAuthenticated, )
 
+
 class CartItemDelView(generics.DestroyAPIView):
-    queryset = CartItem.objects.all()
-    serializer_class = CartItemSerializer
+    permission_classes = (permissions.IsAuthenticated, )
+
+    def delete(self, request, pk, format=None):
+        user = request.user
+        cart_item = CartItem.objects.filter(user=user)
+        target_product = get_object_or_404(cart_item, pk=pk)
+        product = get_object_or_404(Product, id=target_product.product.id)
+        product.quantity = product.quantity + target_product.quantity
+        product.save()
+        target_product.delete()
+        return Response(status=status.HTTP_200_OK, data={"detail": "deleted"})
+
+class CartItemAddOneView(APIView):
+    permission_classes = (permissions.IsAuthenticated, )
+
+    def get(self, request, pk, format=None):
+        user = request.user
+        cart_item = CartItem.objects.filter(user=user)
+        target_product = cart_item.get(pk=pk)
+        product = get_object_or_404(Product, id=target_product.product.id)
+        if product.quantity <= 0:
+            return Response(data={"detail": "this item is sold out try another one !", "code": "sold_out"})  
+        
+        target_product.quantity = target_product.quantity + 1
+        product.quantity = product.quantity - 1
+        product.save()
+        target_product.save()
+        return Response(status=status.HTTP_226_IM_USED,
+            data={
+            "detail": 'one object added', 
+            "code": "done"
+            })
+
+class CartItemReduceOneView(APIView):
+    permission_classes = (permissions.IsAuthenticated, )
+
+    def get(self, request, pk, format=None):
+        user = request.user
+        cart_item = CartItem.objects.filter(user=user)
+        target_product = cart_item.get(pk=pk)
+        product = get_object_or_404(Product, id=target_product.product.id)
+        if target_product.quantity == 0:
+            return Response(data={"detail": "there is no more item like this in tour cart", "code": "no_more"})  
+        
+        target_product.quantity = target_product.quantity - 1
+        product.quantity = product.quantity + 1
+        product.save()
+        target_product.save()
+        return Response(status=status.HTTP_226_IM_USED,
+            data={
+            "detail": 'one object deleted', 
+            "code": "done"
+            })
