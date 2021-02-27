697f4ca52a382bf37465f7e1bd5a282bfc14c199
Faran Taghavi <farantgh@gmail.com>
2021-02-27 17:05:39 -0500

    fixed user delete object && added forgot password reset through the email feature

--- auth/views.py
+++ auth/views.py
@@ -1,15 +1,21 @@
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework_simplejwt.token_blacklist.models import OutstandingToken, BlacklistedToken
-from django.contrib.auth.models import User
-from .serializers import RegisterSerializer, ChangePasswordSerializer, UpdateProfileSerializer, UpdateUserImageSerializer
-from rest_framework import generics
+from rest_framework import generics, validators, status
 from rest_framework.views import APIView
-from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
-from rest_framework import status
-from api.models import Profile
+
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
+from django.core.validators import validate_email
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+
+from api.models import Profile
+from .serializers import RegisterSerializer, ChangePasswordSerializer, UpdateProfileSerializer, UpdateUserImageSerializer
+
+import random
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -44,8 +50,8 @@ class LogoutView(APIView):
         except Exception as e:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
-# TODO: fix the forin key constrant faild error on delete user object
-class DeleteProfile(APIView):
+
+class DeleteProfileView(APIView):
     permission_classes = (IsAuthenticated,)
     def delete(self, request, pk, format=None):
         user = request.user
@@ -55,9 +61,58 @@ class DeleteProfile(APIView):
             return Response(status=status.HTTP_400_BAD_REQUEST, data={'detail': 'password-required'})
         if not user.check_password(request.data['password']):
             return Response(status=status.HTTP_403_FORBIDDEN, data={'detail': "password-incorrect"})
-        tokens = OutstandingToken.objects.filter(user__id=user.id)
-        for token in tokens:
-            t, n = BlacklistedToken.objects.get_or_create(token=token)
-
-        user.delete()
+        user.is_active = False
+        user.save()
         return Response(status=status.HTTP_200_OK, data={"detail": "deleted"})
+
+class ForgotPasswordView(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request, format=None):
+        if not 'email' or 'username' in request.data:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={'detail': {'email_or_username': 'required'}})
+        user = None
+        if 'email' in request.data:
+            user = get_object_or_404(User, email=request.data['email'])
+        if 'user' in request.data:
+            user = get_object_or_404(User, username=request.data['username'])
+        mail_subject = 'Reset Your Password'
+        server_code = random.randint(10000, 999999)
+        message = 'Hi {0},\nthis is your email confirmation code:\n{1}'.format(user.first_name, server_code)
+            
+        to_email = user.email
+        send_email = EmailMessage(mail_subject, message, to=[to_email]).send()
+        request.session['code'] = server_code
+        request.session['user'] = user.username
+        return Response(status=status.HTTP_200_OK, data={'detail': "sent"})
+
+class ValidateConfirmationCodeView(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request, format=None):
+        if not request.session.has_key('code') and request.session.has_key('user'):
+            return Response(status=status.HTTP_404_NOT_FOUND, data={'detail': 'session-not-found'})
+        if not 'code' in request.data:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={'detail': {"code": "required"}})
+        if not int(request.data['code']) == int(request.session['code']):
+            return Response(status=status.HTTP_403_FORBIDDEN, data={'detail': 'wrong-code'})
+        return Response(status=status.HTTP_200_OK, data={'pk': get_object_or_404(User, username=request.session['user']).pk})
+
+class ResetPasswordView(APIView):
+    permission_classes = (AllowAny,)
+
+    def put(self, request, pk, format=None):
+        user = get_object_or_404(User, username=request.session['user'])
+        if user.pk != pk:
+            return Response(status=status.HTTP_401_UNAUTHORIZED, data={"detail": "unauthorized"})
+        if not 'password' and 'again' in request.data:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={'detail': {"password": "required", 'again': 'required'}})
+        if request.data['password'] != request.data['again']:
+            return Response(status=status.HTTP_404_NOT_FOUND, data={'detail': 'not-matched'})
+        user.set_password(request.data['password'])
+        user.save()
+        return Response(status=status.HTTP_200_OK, data={'detail': 'done'})
+
+
+
+
