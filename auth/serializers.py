b1c11a0106e4bee9d522a0408d784df8e003bb93
Faran Taghavi <farantgh@gmail.com>
2021-03-04 17:13:40 -0500

    beautify

--- auth/serializers.py
+++ auth/serializers.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 from api.models import Profile
 
+
 class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
     password1 = serializers.CharField(write_only=True, required=True, validators=[validate_password])
